082f90d081cc681500dd96fbfda318a546302293
Feligx <feligxgamer@gmail.com>
2023-11-14 21:15:59 -0500

    [ADD] React vite project

---
+++ react/eict-spaces/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Login from "./views/login/Login.jsx"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <>
+        <Login />
+    </>
+  )
+}
+
+export default App
---
+++ react/eict-spaces/src/views/login/Login.jsx
@@ -0,0 +1,35 @@
+import './Login.css'
+import {Button, Col, Container, Form, Input, Label, Row} from "reactstrap";
+import logo from "../../assets/ur_logo2.svg"
+const Login = () => {
+    return (
+        <Container fluid>
+            <Row>
+                <Col md="8" className="d-none d-lg-block login-bg">
+                </Col>
+                <Col xs="12" lg="4" className="d-flex flex-column align-items-center justify-content-center">
+                    <h1>
+                        ¡Bienvenido!
+                    </h1>
+                    <span>
+                        Reserva de espacios
+                    </span>
+
+                    <img className="my-5 login-logo" src={logo}/>
+
+                    <Button color="danger" className="rounded-pill">
+                        Iniciar sesión con cuenta UR
+                    </Button>
+                    <Form className="form-check d-flex justify-content-center mt-2">
+                        <Input type="checkbox" className="form-check-input" value="" id="terms"/>
+                        <Label className="form-check-label ms-2" for="terms">
+                            He leido y acepto los <a href="#">términos y condiciones</a>
+                        </Label>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Login
