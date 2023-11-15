77523750a82ab978f2d17e7c5f1766de1c23719b
Camilo Fernandez <62482962+DarkfireT09@users.noreply.github.com>
2023-11-15 13:03:50 -0500

    cards

--- react/eict-spaces/src/App.jsx
+++ react/eict-spaces/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
+import Dash from './views/dashboard/Dash'
 import Login from "./views/login/Login.jsx"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-        <Login />
-    </>
+    <Router>
+        {/* <Login /> */}
+        <Dash />
+    </Router>
   )
 }
 
