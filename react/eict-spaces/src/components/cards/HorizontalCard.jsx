8d1359cb8337e0ccc14bef2ef6574c3eef6e97fe
Guillermo-Ribero <guillermo.ribero@urosario.edu.co>
2023-11-18 18:59:23 -0500

    various test

--- react/eict-spaces/src/components/cards/HorizontalCard.jsx
+++ react/eict-spaces/src/components/cards/HorizontalCard.jsx
@@ -1,33 +1,37 @@
-import { Card, CardBody, CardImg, CardText, CardTitle, Col, Row } from "reactstrap"
+import { Card, CardBody, CardImg, CardText, CardTitle, Col, Row,Container } from "reactstrap"
 import './HorizontalCard.css'
 import { Link } from 'react-router-dom'
 import CardStat from "./CardStat"
 //el link envia a la modal
 const HorizontalCard = ({ nombreSalon, profilePic, person, image, linkModal, reason }) => {
     return (
-        <Link to={linkModal}> 
-            <Row className="card mb-3 row g-0">
-                <Col className="col-2">
-                    <CardImg src={profilePic} className="card-img-top rounded-3 foto-perfil"></CardImg>
-                </Col>
+        <Card className="card mb-3">    
+            <Link to={linkModal}> 
+                <Row className="row g-0">
 
-                <Col className="col-6">
-                    <Row>
-                        <CardTitle className="card-title">
-                            {nombreSalon}
-                        </CardTitle>
-                        <CardText className="mt-0">
-                            {reason}
-                        </CardText>                        
-                        <CardText className="mt-0">{person}</CardText>
-                    </Row>
-                </Col>
+                        <Container  className="card-img-top rounded-3 foto-perfil">
+                            <img src={profilePic}></img>
+                        </Container>
 
-                <Col className="col-4 foto-lab">
-                    <CardImg src={image} className="card-img-top rounded-3"></CardImg>
-                </Col>
-            </Row>
-        </Link>
+
+                    <Col className="col-6">
+                        <Row>
+                            <CardTitle className="card-title">
+                                {nombreSalon}
+                            </CardTitle>
+                            <CardText className="mt-0">
+                                {reason}
+                            </CardText>                        
+                            <CardText className="mt-0">{person}</CardText>
+                        </Row>
+                    </Col>
+
+                    <Col className="col-4 foto-lab">
+                        <CardImg src={image} className="card-img-top rounded-3"></CardImg>
+                    </Col>
+                </Row>
+            </Link>
+        </Card>
     )
 }
 
