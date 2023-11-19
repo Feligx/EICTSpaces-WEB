334e31cb180694d903e9d47630244789f1a8be14
Camilo Fernandez <62482962+DarkfireT09@users.noreply.github.com>
2023-11-18 19:59:37 -0500

    dashboard

--- react/eict-spaces/src/components/cards/VerticalCard.jsx
+++ react/eict-spaces/src/components/cards/VerticalCard.jsx
@@ -1,20 +1,20 @@
-import { Card, CardBody, CardImg, CardText } from "reactstrap"
+import { Card, CardBody, CardImg, CardText, Button } from "reactstrap"
 import './VerticalCard.css'
 import { Link } from 'react-router-dom'
 import CardStat from "./CardStat"
 
 const VerticalCard = ({ title, place, people, pc, image, link }) => {
     return (
-        <Link to={link}>
-            <Card className="ca ms-4 mt-1 rounded-3 px-0">
-                <CardImg src={image} className="card-img-top rounded-3"></CardImg>
+        <Button className="link me-4 ms-5" onClick={link}>
+            <Card className="ca rounded-3 px-0">
+                <CardImg src={image} className="c_img card-img-top rounded-3"></CardImg>
                 <CardBody>
                     <CardText className="fs-2">{title}</CardText>
                     <CardText className="mt-0">{place}</CardText>
                     <CardStat people={people} pc={pc} />
                 </CardBody>
             </Card>
-        </Link>
+        </Button>
     )
 }
 
--- react/eict-spaces/src/views/dashboard/Dash.jsx
+++ react/eict-spaces/src/views/dashboard/Dash.jsx
@@ -1,12 +1,47 @@
+import React, {useState} from 'react';
 import VerticalCard from '../../components/cards/VerticalCard';
+import SearchBar from '../../components/search_bar/SearchBar';
 import './Dash.css'
-import {Button, Col, Container, Form, Input, Label, Row} from "reactstrap";
+import { Button, Col, Container, Form, Input, Label, Row } from "reactstrap";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const Dash = () => {
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
     return (
-        <Row>
-            <VerticalCard title="EICT Spaces" place="EICT Building" people="10" pc="10" image="https://picsum.photos/200/300" link="/"/>
-        </Row>
+        <Container fluid className='vh-100 d-flex align-items-start flex-column'>
+            <Row className='w-100 mb-5 mt-4'>
+                <Col className='search-bar'>
+                    <SearchBar />
+                </Col>
+            </Row>
+            <Row className='w-100'>
+                <VerticalCard title="EICT Spaces" place="EICT Building" people="10" pc="10" image="https://picsum.photos/200/300" link="/" />
+                <VerticalCard title="EICT Spaces" place="EICT Building" people="10" pc="10" image="https://picsum.photos/200/300" link={toggle} />
+            </Row>
+
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+                <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={toggle}>
+                        Do Something
+                    </Button>{' '}
+                    <Button color="secondary" onClick={toggle}>
+                        Cancel
+                    </Button>
+                </ModalFooter>
+            </Modal>
+        </Container>
     )
 }
 
