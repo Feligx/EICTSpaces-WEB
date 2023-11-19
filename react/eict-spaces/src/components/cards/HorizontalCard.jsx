2c87d0332478e763900d0b975d9acbc27b4ed483
Guillermo-Ribero <guillermo.ribero@urosario.edu.co>
2023-11-18 21:10:50 -0500

    used variables to change between modals

--- react/eict-spaces/src/components/cards/HorizontalCard.jsx
+++ react/eict-spaces/src/components/cards/HorizontalCard.jsx
@@ -1,34 +1,110 @@
-import { Card, CardBody, CardImg, CardText, CardTitle, Col, Row,Container } from "reactstrap"
+import { Card, Button, CardImg, CardText, CardTitle, Col, Row, Modal, ModalHeader, ModalFooter, ModalBody } from "reactstrap"
 import './HorizontalCard.css'
-import { Link } from 'react-router-dom'
-import CardStat from "./CardStat"
-import ModalBookings1 from '../../components/cards/ModalBookings1'
+import {useState} from "react";
 //el link envia a la modal
-const HorizontalCard = ({ nombreSalon, profilePic, person, image, linkModal, reason }) => {
+const HorizontalCard = ({ nombreSalon, profilePic, person, image, reason, userAdmin }) => {//userAdmin es un booleano que indica si el usuario es admin o no para las modales
+    const [modal, setModal] = useState(false);
+    const [modal2, setModal2] = useState(false);
+    const toggle = () => setModal(!modal);
+    const toggle2 = () => setModal2(!modal2);
+    let togs = userAdmin==true ? toggle : toggle2
     return (
-        <Card className="card mb-3">    
-            <Link to={linkModal}> 
-                <Row className="row g-0">
-                    <Col  className="card-img-top rounded-3 foto-perfil">
-                        <img src={profilePic}></img>
-                    </Col>
-                    <Col className="col-6">
-                        <Row>
-                            <CardTitle className="card-title">
-                                {nombreSalon}
-                            </CardTitle>
-                            <CardText className="mt-0">
-                                {reason}
-                            </CardText>                        
-                            <CardText className="mt-0">{person}</CardText>
+        <>
+            <Button onClick = {togs}  className="w-100 hori-card"> 
+                <Card className="card mb-3">   
+                        <Row className="row g-0">
+                            <Col  className="card-img-top rounded-3 foto-perfil">
+                                <img src={profilePic}></img>
+                            </Col>
+                            <Col className="col-6">
+                                <Row>
+                                    <CardTitle className="card-title">
+                                        {nombreSalon}
+                                    </CardTitle>
+                                    <CardText className="mt-0">
+                                        {reason}
+                                    </CardText>                        
+                                    <CardText className="mt-0">{person}</CardText>
+                                </Row>
+                            </Col>
+                            <Col className="col-4 foto-lab">
+                                <CardImg src={image} className="card-img-top rounded-3"></CardImg>
+                            </Col>
                         </Row>
-                    </Col>
-                    <Col className="col-4 foto-lab">
-                        <CardImg src={image} className="card-img-top rounded-3"></CardImg>
-                    </Col>
-                </Row>
-            </Link>
-        </Card>
+                </Card>
+            </Button>
+            <div className="d-flex mb-5">
+                <Modal isOpen={modal} toggle={toggle}>
+                    <ModalHeader toggle={toggle}>
+                        <h4 className="modal-title">
+                        Salon Ada Lovelace
+                        </h4>
+                        <h4 className="modal-title">
+                        Proyecto clase de programación
+                        </h4>
+                    </ModalHeader>
+                    <ModalBody>
+                        <h5>
+                        Horarios:
+                        </h5>
+                        <ul>
+                        <li>
+                            1/1/24, 7:00 am - 9:00 am
+                        </li>
+
+                        <li>
+                            8/1/24, 7:00 am - 9:00 am
+                        </li>
+                        </ul>
+
+                        <h6>Pido uso del salon Lovelace con el fin de poder adelantar mi proyecto de la clase de programación de computadores </h6>
+                    
+                    </ModalBody>
+                    <ModalFooter>
+                    <Button color="secondary" onClick={toggle} className="btn btn-outline-success">
+                        Cancelar mi reserva
+                    </Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+            <div className="d-flex mb-5">
+                <Modal isOpen={modal2} toggle={toggle2}>
+                    <ModalHeader toggle={toggle2}>
+                        <h4 className="modal-title">
+                        Salon Ada Lovelace
+                        </h4>
+                        <h4 className="modal-title">
+                        Proyecto clase de programación
+                        </h4>
+                    </ModalHeader>
+                    <ModalBody>
+                        <h5>
+                        Horarios:
+                        </h5>
+                        <ul>
+                        <li>
+                            1/1/24, 7:00 am - 9:00 am
+                        </li>
+
+                        <li>
+                            8/1/24, 7:00 am - 9:00 am
+                        </li>
+                        </ul>
+
+                        <h6>Pido uso del salon Lovelace con el fin de poder adelantar mi proyecto de la clase de programación de computadores </h6>
+                    
+                    </ModalBody>
+                    <ModalFooter>
+                    <Button color="primary" onClick={toggle2} className="btn btn-secondary">
+                        Rechazar
+                    </Button>{' '}
+                    <Button color="secondary" onClick={toggle2} className="btn btn-outline-success">
+                        Aceptar
+                    </Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        </>
     )
 }
 
--- react/eict-spaces/src/views/bookings/Bookings.jsx
+++ react/eict-spaces/src/views/bookings/Bookings.jsx
@@ -3,38 +3,32 @@ import HorizontalCard from '../../components/cards/HorizontalCard'
 const Bookings = () => {
     return (
         <div className="container">
-            <HorizontalCard  
+            <HorizontalCard 
                 nombreSalon = "nombreSalon"
                 profilePic = "../../../public/fotoperf.webp"
                 person = "Camilo F"
                 image = "../../../public/space.jpg"
-                linkModal="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwipnbSJ6s6CAxXYfDABHd0cAd8QyCl6BAgbEAM&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU&opi=89978449"
                 reason = "para ver bochi" 
+                userAdmin = {false}
             />
-            <HorizontalCard  
+            <HorizontalCard
                 nombreSalon = "nombreSalon"
                 profilePic = "../../../public/fotoperf.webp"
                 person = "Camilo F"
                 image = "../../../public/space.jpg"
-                linkModal="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwipnbSJ6s6CAxXYfDABHd0cAd8QyCl6BAgbEAM&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU&opi=89978449"
                 reason = "para ver bochi" 
+                userAdmin = {true}
             />
-            <HorizontalCard  
+
+
+            <HorizontalCard
                 nombreSalon = "nombreSalon"
                 profilePic = "../../../public/fotoperf.webp"
                 person = "Camilo F"
                 image = "../../../public/space.jpg"
-                linkModal="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwipnbSJ6s6CAxXYfDABHd0cAd8QyCl6BAgbEAM&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU&opi=89978449"
                 reason = "para ver bochi" 
             />
-            <HorizontalCard  
-                nombreSalon = "nombreSalon"
-                profilePic = "../../../public/fotoperf.webp"
-                person = "Camilo F"
-                image = "../../../public/space.jpg"
-                linkModal="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwipnbSJ6s6CAxXYfDABHd0cAd8QyCl6BAgbEAM&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU&opi=89978449"
-                reason = "para ver bochi" 
-            />
+
         </div>
     )
 }
