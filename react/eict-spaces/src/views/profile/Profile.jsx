ea7cb30997db2ff629eea0d52f4df368445d1554
dleyvacastro <62482485+dleyvacastro@users.noreply.github.com>
2023-11-26 12:17:01 -0500

    move off canvas to dashboard

--- react/eict-spaces/src/views/profile/Profile.jsx
+++ react/eict-spaces/src/views/profile/Profile.jsx
@@ -3,17 +3,12 @@ import {Envelope, Funnel, Power, Telephone} from "react-bootstrap-icons";
 import './Profile.css'
 import InformationCard from "../../components/cards/InformationCard.jsx";
 import {useState} from "react";
-import Form from "react-bootstrap/Form";
-
 
 
 
 const Profile = ({name="John Doe"}) => {
     const [modal, setModal] = useState(false);
     const toggleModal = () => setModal(!modal);
-
-    const [showOffcanvas, setShowOffcanvas] = useState(false);
-    const toggleOffcanvas = () => setShowOffcanvas(!showOffcanvas);
 
     return (
         <Row className="d-flex align-items-start vh-100 w-100 align-content-between">
@@ -55,27 +50,6 @@ const Profile = ({name="John Doe"}) => {
                 >
                     Open
                 </Button>
-
-                <Offcanvas isOpen={showOffcanvas} toggle={toggleOffcanvas} direction="end" className='filter'>
-                    <OffcanvasHeader toggle={toggleOffcanvas}>
-                        <Funnel/> Filtros
-                    </OffcanvasHeader>
-                    <OffcanvasBody >
-                        <Form.Label> Capacidad </Form.Label>
-                        <Form.Range />
-                        <Form.Label> Equipos de computo </Form.Label>
-                        <Form.Range />
-
-                        <Form.Label> Tipo de salon </Form.Label>
-                        <Form.Select aria-label="Default select example" className='dropdown'>
-                            <option>Seleccione un tipo</option>
-                            <option value="1">Tipo 1</option>
-                            <option value="2">Tipo 2</option>
-                            <option value="3">Tipo 3</option>
-                        </Form.Select>
-
-                    </OffcanvasBody>
-                </Offcanvas>
 
 
             </div>
